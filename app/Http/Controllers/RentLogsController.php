5012fafaa640e53b468378751318d42c1c2f9ae7
ReygaDirga <reygadirg@gmail.com>
2025-09-25 15:47:34 +0700

    update mcr

---
+++ app/Http/Controllers/RentLogsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\rent_logs;
+use Illuminate\Http\Request;
+
+class RentLogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(rent_logs $rent_logs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(rent_logs $rent_logs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, rent_logs $rent_logs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(rent_logs $rent_logs)
+    {
+        //
+    }
+}
